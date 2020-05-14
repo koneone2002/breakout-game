7e77849601a0c3ffaaeab26214801abbf7a023f9
koneone2002 <katieo3507@gmail.com>
2020-05-14 11:29:08 -0700

    added paddle movement and wall detection

--- script.js
+++ script.js
@@ -78,8 +78,23 @@ function drawBricks() {
     });
   });
 }
+// move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
 // Draw everything
 function draw() {
+  //  clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
   drawPaddle();
   drawScore();
@@ -91,7 +106,38 @@ function drawScore() {
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
-draw();
+// update canvas drawing and animation
+function update() {
+  movePaddle();
+
+  // draw everything
+  draw();
+  requestAnimationFrame(update);
+}
+update();
+// keydown event
+function keyDown(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed;
+  }
+}
+// keyup event
+function keyUp(e) {
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0;
+  }
+}
+
+// Keyboard events
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 // Event listeners
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
