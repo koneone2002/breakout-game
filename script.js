89b0dc6b1fbf064c394ec66eca3f4038d2825203
koneone2002 <katieo3507@gmail.com>
2020-05-14 11:03:05 -0700

    added in tiles on canvas

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const closeBtn = document.getElementById('close-btn');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // Init canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -44,12 +47,43 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
-
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true
+};
+// create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+// Draw Bricks on canvas
+function drawBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
 // Draw everything
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
+  drawBricks();
 }
 // Draw score on canvas
 function drawScore() {
